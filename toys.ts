e37b70eec5229f1c75a3e89d28a80c644f841a43
BwgItQa <123381391+BwgItQa@users.noreply.github.com>
2023-01-23 16:41:16 +0300

    Lesson2

---
+++ toys.ts
@@ -0,0 +1,18 @@
+
+
+//numbers
+console.log('Сложение')
+console.log(1+190+5678+987+46325)
+
+console.log('Вычитание')
+console.log(187-1900-1500-32)
+
+console.log('Умножение')
+console.log(1657*1923*100000*457)
+
+//Строки
+var string_1 = "Hello"
+var string_2 = "World"
+var final_string = string_1 + " "+ string_2
+console.log("Строки")
+console.log(final_string)
